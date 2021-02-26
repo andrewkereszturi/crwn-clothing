757e7944f517d473e9f3e5e699ff5cca6bad06aa
akereszturi <andrew.kereszturi@gmail.com>
2021-02-26 09:06:41 -0800

    Stripe Integration (depreciated) and update favicon / title

---
+++ src/components/stripe-button/stripe-button-component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51IORzxHxT67BnRH9kn7Nh8SwlTvKuwP9Tt5UOnPZs56xSgdOotAmEvPqaiD0G1TXN4MPmfjcKwZtGnvFlhqDMfAs005xlkXf0o';
+
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
